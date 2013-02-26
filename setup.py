80e04000e6e37595652f3bb45eedfa47f7fff0c6
Aaron Westendorf <aaron@agoragames.com>
2013-02-26 17:05:42 -0500

    Project boilerplate

---
+++ setup.py
@@ -0,0 +1,39 @@
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+requirements = [req.strip() for req in open('requirements.pip')]
+if sys.version_info[:2] <= (2, 6):
+    requirements = [req.strip() for req in open('requirements_py26.pip')]
+
+exec([v for v in open('torus/__init__.py') if '__version__' in v][0])
+
+setup(
+    name='torus',
+    version=__version__,
+    author='Aaron Westendorf',
+    author_email="aaron@agoragames.com",
+    packages = ['torus'],
+    install_requires = requirements,
+    url='https://github.com/agoragames/torus',
+    license="LICENSE.txt",
+    description='Carbon and Graphite replacement using Kairos for timeseries storage',
+    long_description=open('README.md').read(),
+    keywords=['python', 'redis', 'time series', 'statistics'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        "Intended Audience :: Developers",
+        "Operating System :: POSIX",
+        "Topic :: Communications",
+        "Topic :: System :: Distributed Computing",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries'
+    ]
+)
---
+++ torus/__init__.py
@@ -0,0 +1,6 @@
+'''
+Copyright (c) 2013, Agora Games, LLC All rights reserved.
+
+https://github.com/agoragames/kairos/blob/master/LICENSE.txt
+'''
+__version__ = "0.0.0"
